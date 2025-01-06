9949f1bd83215ebbbca5e27d8306bebbe825f9c6
dog <panwan.21st@gmail.com>
2025-01-06 18:33:20 +0700

    update -rewrite

--- main.js
+++ main.js
@@ -1,150 +1,133 @@
-const mongoose = require('mongoose')
-const ShortUrlmodel = require('./models/shortUrl')
-const express = require("express")
-const app = express()
-require('dotenv').config()
-port = process.env.PORT
-dburl = process.env.DBURL
-app.use(express.urlencoded({ extended: false }))
-app.set('view engine','ejs')
+const mongoose = require('mongoose');
+const ShortUrlmodel = require('./models/shortUrl');
+const express = require("express");
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT;
+const dburl = process.env.DBURL;
+
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect(dburl, {useNewUrlParser: true, useUnifiedTopology: true})
+// Connect to MongoDB
+mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to database'))
+  .catch(err => console.error('Database connection error:', err));
 
-
-
-shortlink = ""
-
-app.get('/',async(req,res)=>{
-    // const shortUrls = await ShortUrlmodel.find()
-    res.render('./index.ejs',{shortUrls : ""});
-    // res.render("./index")
-})
-
-app.post('/generators', async(req,res)=>{
-    texturl = req.body.texturl
-    if(!texturl.includes("https://")){
-      texturl = "https://"+texturl
-    }
-   console.log(isURL(texturl));
-    if(!isURL(texturl)){
-      res.render('./index',{shortUrls:false});
-    }else{
-      const shortUrls = await ShortUrlmodel.findOne({ full: texturl })
-      // console.log(shortUrls+"<<<<");
-      if(shortUrls == null){
-        let newshortUrls = await ShortUrlmodel.create({ full: texturl })
-        res.render('./index',{shortUrls:newshortUrls.short});
-        // console.log(newshortUrls.short+"dddddddddd");
-        // res.redirect('/')
-      }else{
-        // console.log(shortUrls);
-        res.render('./index',{shortUrls:shortUrls.short});
-      }
-    } 
-})
-
-
-app.get('/tracker', async (req, res) => {
-  const shortUrls = await ShortUrlmodel.find()
-  res.render('./history',{URLTrackers:shortUrls})
-})
-
-
-app.get('/topurl', async(req,res)=>{
-  const shortUrls = await ShortUrlmodel.find()
-  let full = []
-  let countclick = []
-  shortUrls.forEach(item => {
-    if(!full.includes(getbaseurl(item.full).replace("https://",""))){
-      full.push(getbaseurl(item.full).replace("https://",""))
-    }
-   
-  });
-  let i = 0
-  for (let item in full){
-      let count = 0
-      shortUrls.forEach(item =>{
-        if (full[i] == getbaseurl(item.full).replace("https://","")){
-          count += item.clicks;
-          // console.log("shuobj = "+ item.clicks);
-        }
-      });
-      countclick.push(count)
-      i++;
-  }
-  
-  var dict = [];
-  full.forEach((item,index)=>{
-    dict.push([item,countclick[index]])
-
-  })
-
-  dict.sort(function(a, b) {
-    return b[1] - a[1];
+app.get('/', async (req, res) => {
+  res.render('index', { shortUrls: [] });
 });
 
-let objSorted = {}
-dict.forEach(function(item){
-    objSorted[item[0]]=item[1]
-})
+app.post('/generators', async (req, res) => {
+  let texturl = req.body.texturl;
 
-  
- 
-  
-  res.render('./topurl',{data:{full:full,countclick:countclick,sortdict:objSorted}})
-})
-
-
-app.get('/:shortid', async (req, res) => {
-  const shortUrl = await ShortUrlmodel.findOne({ short: req.params.shortid })
-  if (shortUrl == null) return res.sendStatus(404)
-  shortUrl.clicks++
-  shortUrl.save()
-
-  res.redirect(shortUrl.full)
-})
-
-
-
-app.listen(port,()=>{
-    console.log(`App listen port ${port}`);
-})
-
-
-
-
-  function isURL(str) {
-    try{
-      getbaseurl(str)
-      var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return !!pattern.test(str);
-    }catch(err) {
-      console.log(err);
-      return false
-    }
-    
+  if (!texturl.includes("https://")) {
+    texturl = "https://" + texturl;
   }
 
-  function getbaseurl(str){
-    if(str.includes("https://")){
-        let url = new URL(str);
-        // console.log(url);
-        return url.origin;
-    }else {
-        let url = new URL("https://"+str);
-        return url.origin;
-    }
-    
-}
-//   function getbaseurl(str){
-//     let url = new URL(str);
-//     // console.log(url+"<<url");
-//     return url.origin;
-// }
+  if (!isURL(texturl)) {
+    return res.render('index', { shortUrls: false });
+  }
 
+  try {
+    const existingShortUrl = await ShortUrlmodel.findOne({ full: texturl });
+
+    if (!existingShortUrl) {
+      const newShortUrl = await ShortUrlmodel.create({ full: texturl });
+      return res.render('index', { shortUrls: newShortUrl.short });
+    }
+
+    return res.render('index', { shortUrls: existingShortUrl.short });
+  } catch (err) {
+    console.error('Error processing URL:', err);
+    res.render('index', { shortUrls: false });
+  }
+});
+
+app.get('/tracker', async (req, res) => {
+  try {
+    const shortUrls = await ShortUrlmodel.find();
+    res.render('history', { URLTrackers: shortUrls });
+  } catch (err) {
+    console.error('Error fetching tracking data:', err);
+    res.render('history', { URLTrackers: [] });
+  }
+});
+
+app.get('/topurl', async (req, res) => {
+  try {
+    const shortUrls = await ShortUrlmodel.find();
+    let full = [];
+    let countclick = [];
+
+    shortUrls.forEach(item => {
+      const baseUrl = getBaseUrl(item.full);
+      if (!full.includes(baseUrl)) {
+        full.push(baseUrl);
+      }
+    });
+
+    full.forEach((item, index) => {
+      let count = 0;
+      shortUrls.forEach(shortUrl => {
+        if (getBaseUrl(shortUrl.full) === item) {
+          count += shortUrl.clicks;
+        }
+      });
+      countclick.push(count);
+    });
+
+    const sortedDict = full.map((item, index) => [item, countclick[index]])
+      .sort((a, b) => b[1] - a[1])
+      .reduce((obj, [key, value]) => {
+        obj[key] = value;
+        return obj;
+      }, {});
+
+    res.render('topurl', { data: { full, countclick, sortdict: sortedDict } });
+  } catch (err) {
+    console.error('Error fetching top URLs:', err);
+    res.render('topurl', { data: { full: [], countclick: [], sortdict: {} } });
+  }
+});
+
+app.get('/:shortid', async (req, res) => {
+  try {
+    const shortUrl = await ShortUrlmodel.findOne({ short: req.params.shortid });
+
+    if (!shortUrl) {
+      return res.sendStatus(404);
+    }
+
+    shortUrl.clicks++;
+    await shortUrl.save();
+    res.redirect(shortUrl.full);
+  } catch (err) {
+    console.error('Error redirecting short URL:', err);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+function isURL(str) {
+  try {
+    new URL(str);  // Improved URL validation
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+function getBaseUrl(str) {
+  try {
+    const url = new URL(str);
+    return url.origin;
+  } catch (err) {
+    return '';  // Return empty string for invalid URLs
+  }
+}
